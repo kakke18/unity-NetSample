107591954d182142c0560d220b2670c5560f6e0d
kakke18 <tanaka-kakeru-xm@ynu.jp>
2018-06-26 16:02:27 +0900

    Add Client.cs

---
+++ Assets/Client.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string ipOrHost = "localhost";
+		int port = 10000;
+
+		TcpClient tcp = new TcpClient(ipOrHost, port);
+		Debug.Log("success");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
